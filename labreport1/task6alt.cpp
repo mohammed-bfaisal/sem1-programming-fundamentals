ff8458ee010a398a11734d72ee6a5860f2e8c702
Mohammed Bin Faisal <munertfs@gmail.com>
2026-09-16 15:02:24 +0500

    add task6 alt method

---
+++ labreport1/task6alt.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Full Pyramid" << endl;
+    cout << "               *                   " << endl;
+    cout << "              * *                  " << endl;
+    cout << "             * * *                 " << endl;
+    cout << "            * * * *                " << endl;
+    cout << "           * * * * *               " << endl;  
+    cout << "Inverted full Pyramid" << endl;
+    cout << "           * * * * *               " << endl;
+    cout << "            * * * *                " << endl;
+    cout << "             * * *                 " << endl;
+    cout << "              * *                  " << endl; 
+    cout << "               *                   " << endl;
+    cout << "Hollow Full Pyramid" << endl;
+    cout << "               *                   " << endl;
+    cout << "              * *                  " << endl;
+    cout << "             *   *                 " << endl;
+    cout << "            *     *                " << endl;
+    cout << "           *       *               " << endl;
+    cout << "          *         *              " << endl;
+    cout << "         *           *             " << endl;
+    cout << "        ***************            " << endl;
+}
