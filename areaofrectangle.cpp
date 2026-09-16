c3fea64c1fac059118c8665c4518aad1dc92f5b2
Mohammed Bin Faisal <munertfs@gmail.com>
2026-09-16 10:09:09 +0500

    Fix input prompts and correct area calculation in areaofrectangle.cpp

--- areaofrectangle.cpp
+++ areaofrectangle.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main() {
     int l, w, a;
-    cout << "Enter the length of the rectangle/plot";
+    cout << "Enter the length of the rectangle/plot: ";
     cin >> l;
-    cout << "Enter the width of the rectangle/plot";
+    cout << "Enter the width of the rectangle/plot: ";
     cin >> w;
-    a = l+w;
+    a = l*w;
     cout << "Area is" << a << endl;
 }
