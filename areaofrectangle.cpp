bcb0742aa8128c47b981b552808f9e4538dc2988
Mohammed Bin Faisal <munertfs@gmail.com>
2026-09-16 10:07:58 +0500

    Add area calculation for rectangle in areaofrectangle.cpp

---
+++ areaofrectangle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int l, w, a;
+    cout << "Enter the length of the rectangle/plot";
+    cin >> l;
+    cout << "Enter the width of the rectangle/plot";
+    cin >> w;
+    a = l+w;
+    cout << "Area is" << a << endl;
+}
