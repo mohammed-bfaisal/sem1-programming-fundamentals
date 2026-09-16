3cfd5344b647d4a99058debad708f91d5fd8741c
Mohammed Bin Faisal <munertfs@gmail.com>
2026-09-16 11:42:49 +0500

    update file

--- labclass2/lab2task2.cpp
+++ labclass2/lab2task2.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 
 int main() {
-    cout << "1 \t 2 \t 1 \t 4 \n \t 2 \t 2 \n 3  1 \t |  4";
+    cout << "1 \t \t 2 \t 1 \t 4 \n \t      2      2 \n 3    1 \t \t 1     4";
     return 0;
 }
